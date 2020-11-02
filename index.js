04db8278feccd4bf68a7897e8c267cb6c75dfc59
openssfrepo <sonnet.amit@gmail.com>
2020-11-02 23:50:17 +0900

    socket.io add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const index = require("./src/routes/api");
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 4001;
 const app = express();
+app.use(express.static("src/public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(index);
@@ -13,13 +14,20 @@ const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-io.on('connection',function(socket){
-    socket.on('message',function(msg){
+// io.on('connection',function(socket){
+//     socket.on('message',function(msg){
 
-        console.log('message: ' + msg);
+//         console.log('message: ' + msg);
 
-        io.emit('message', msg);
+//         io.emit('message', msg);
+//     });
+// });
+io.on("connection", socket => {
+    console.log("a user connected :D");
+    socket.on("chat message", msg => {
+      msg= "server "+msg;
+      console.log(msg);
+      io.emit("chat message", msg);
     });
-});
-
+  });
 server.listen(port, () => console.log(`Listening on port ${port}`));
